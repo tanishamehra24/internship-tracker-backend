614fa64709f23032aa0e21f8dd1cf74b0a58d599
tanishamehra24 <tanishamehra24@gmail.com>
2026-02-19 12:15:23 +0530

    added cors import

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express"; //imported express
 import applicationsRoutes from "./routes/applications.routes.js"; //imported routes
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/applications", applicationsRoutes); //mounting routes
 
