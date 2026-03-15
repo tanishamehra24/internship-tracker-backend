e906c098bd0895284d262707f29c064481aaccfd
tanishamehra24 <tanishamehra24@gmail.com>
2026-03-15 14:00:21 +0530

    changed database url

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./app.js";
 import "./db/db.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 //starting the server...
 app.listen(PORT, () => {
   console.log(`Server is listening on Port ${PORT}...`);
