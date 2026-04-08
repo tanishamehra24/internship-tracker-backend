ac2875941775e36b0bf94ec81df13009a7573927
tanishamehra24 <tanishamehra24@gmail.com>
2026-04-08 12:02:40 +0530

    temp ping to check the sever

--- src/app.js
+++ src/app.js
@@ -5,14 +5,28 @@ import cors from "cors";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://interntrack-mu.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(cors({
-  origin: "https://interntrack-mu.vercel.app/",
+  origin: "https://interntrack-mu.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(express.json());
+// temp debug route
+app.get("/ping", (req, res) => res.json({ message: "Server is alive!" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/applications", applicationsRoutes); //mounting routes
 
