7a6ef472ba1932e9e59ff53876dbea7c2e9ae4ad
tanishamehra24 <tanishamehra24@gmail.com>
2026-04-10 12:16:17 +0530

    changes in app.js

--- src/app.js
+++ src/app.js
@@ -5,18 +5,6 @@ import cors from "cors";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://interntrack-mu.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 app.use(cors({
   origin: "https://interntrack-mu.vercel.app",
   credentials: true,
@@ -25,9 +13,9 @@ app.use(cors({
 }));
 
 app.use(express.json());
-// temp debug route
-app.get("/ping", (req, res) => res.json({ message: "Server is alive!" }));
+
+app.get("/", (req, res) => res.send("API is running"));
 app.use("/api/auth", authRoutes);
-app.use("/api/applications", applicationsRoutes); //mounting routes
+app.use("/api/applications", applicationsRoutes);
 
 export default app;
